2fea03421898fad9d27133e19ec01d67d9c7c871
Paul-Ben Ajene <paulben.ajene@gmail.com>
2024-07-14 06:31:36 +0100

    Middleware set up for different roles

---
+++ app/Http/Middleware/AcademicOfficer.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AcademicOfficer
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
+
+        if (Auth::user()->role == 'User') {
+            return redirect()->route('dashboard');
+        }
+
+        if (Auth::user()->role == 'Admin') {
+            return redirect()->route('admin');
+        }
+
+        if (Auth::user()->role == 'IT') {
+            return redirect()->route('it');
+        }
+
+        if (Auth::user()->role == 'Bursar') {
+            return redirect()->route('bursary');
+        }
+        if (Auth::user()->role == 'AcademicOfficer') {
+            return $next($request);
+        }
+        if (Auth::user()->role == 'Student') {
+            return redirect()->route('student');
+        }
+        if (Auth::user()->role == 'Teacher') {
+            return redirect()->route('teacher');
+        }
+    }
+}
---
+++ app/Http/Middleware/Student.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class Student
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
+        if(Auth::user()->role == 'User'){
+            return redirect()->route('user');
+        }
+
+        if (Auth::user()->role == 'Admin') {
+            return redirect()->route('admin');
+        }
+
+        if (Auth::user()->role == 'IT'){
+            return redirect()->route('it');
+        }
+
+        if (Auth::user()->role == 'Bursar') {
+            return redirect()->route('bursar');
+        }
+
+        if (Auth::user()->role == 'AcademicOfficer') {
+            return redirect()->route('academicOfficer');
+
+        }
+
+        if (Auth::user()->role == 'Student') {
+            return $next($request);
+        }
+
+        if (Auth::user()->role == 'Teacher') {
+            return redirect()->route('teacher');
+        }
+    }
+}
