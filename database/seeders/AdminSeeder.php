c8b9aaa7d66689e275762488bf15364ae03dd6d2
Paul-Ben <paulben.ajene@gmail.com>
2024-07-19 23:05:47 +0100

    Database seeders Added
    Admin user and Student user accounts added via seeders

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            
+                'name' => 'Admin',
+                'email' => 'admin@admin.com',
+                'password' => bcrypt('admin'),
+                'role' => 'Admin',
+            
+           
+        ]);
+        User::factory()->create([
+            
+            'name' => 'Student',
+            'email' => 'student@mail.com',
+            'password' => bcrypt('student'),
+            'role' => 'Student',
+        
+       
+    ]);
+       
+    }
+}
